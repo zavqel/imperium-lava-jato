bae65f2f06bde6d737a591e099f9ce81e27b04be
zavqel <zavqelparfums@gmail.com>
2026-09-22 21:56:11 -0300

    Delete js directory

--- js/site.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{const b=document.querySelector('.menu-btn'),m=document.querySelector('#menu');if(b&&m)b.addEventListener('click',()=>m.classList.toggle('open'));});
